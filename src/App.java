a4bbf4c972db2cbd7008c26b4fbefc13370561a4
Barret J. Nobel <bear.nobel@gmail.com>
2016-04-27 11:31:39 -0400

    comments removed unnecessary code finished implementation of the copy constructor getWeightDirection method returns HashMap instead of Map now

--- src/App.java
+++ src/App.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * Created by Barret J. Nobel on 4/26/2016.
  */
@@ -11,9 +9,12 @@ public class App {
         Neuron test2 = new Neuron( "test2", 1.0 );
         Neuron test3 = new Neuron( "test3", 1.8 );
 
+
         test1.setAConnectedNodeDirection( "forward", test2 );
         test1.setAConnectedNodeDirection( "forward", test3 );
         System.out.println( test1.getConnectedNodesDirection("forward") );
+        Neuron text4 = new Neuron( test1 );
+        System.out.println( text4.getConnectedNodesDirection("forward") );
 //        test1.setAConnectedWeightDirection( "FORWARD", 2.0 );
 //        System.out.println( test1.getWeightDirection( "FORWARD" ) );
     }
--- src/Neuron.java
+++ src/Neuron.java
@@ -4,46 +4,54 @@
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Neuron implements Cloneable {
-    private double sigmoid;
-    private double threshold;
-    private ArrayList< Neuron > forwardNodes;
-    private ArrayList< Neuron > backwardNodes;
-    private HashMap< String, Double > weightDirection = new HashMap<>();
-    private String name;
+    private double sigmoid; //................................ instance variable to hold sigmoid value
+    private double threshold; //.............................. instance variable to hold the threshold value
+    private ArrayList< Neuron > forwardNodes; //.............. list of all nodes connected forward
+    private ArrayList< Neuron > backwardNodes; //............. list of all nodes connected backwards
+    private HashMap< String, Double > weightDirection; //..... map to give the weight in forward / backward direction
+    private String name; //................................... Neuron name
 
-    private static final String FORWARD = "forward";
-    private static final String BACKWARD = "backward";
+    private static final String FORWARD = "forward"; //....... final value for key in map
+    private static final String BACKWARD = "backward"; //..... final value for key in map
 
-//    public Neuron(){
-//        this.setSigmoid( 0.0 );
-//        this.setThreshold( 0.0 );
-//        forwardNodes = new ArrayList<>();
-//        backwardNodes = new ArrayList<>();
-//        this.name = "";
-//    }
-
+    // Constructor with parameters
+    // requires all new Neurons to have a name and threshold value
     public Neuron( String name, double threshold ){
-        this.setSigmoid( 0.0 );
+        this.setSigmoid( 0.0 ); //................... initialize all instance variables
         this.setThreshold( threshold );
         forwardNodes = new ArrayList<>();
         backwardNodes = new ArrayList<>();
+        weightDirection = new HashMap<>();
         this.name = name;
     }
 
+    // Copy constructor
+    // makes a copy of the Neuron passed in
     public Neuron( Neuron copyMe ){
         this.setSigmoid( copyMe.getSigmoid() );
         this.setThreshold( copyMe.getThreshold() );
+        forwardNodes = new ArrayList<>();
+        backwardNodes = new ArrayList<>();
+        weightDirection = new HashMap<>();
         this.arrayListHelper( copyMe.getConnectedNodesDirection(FORWARD), forwardNodes);
         this.arrayListHelper( copyMe.getConnectedNodesDirection(BACKWARD), backwardNodes);
+        this.hashMapHelper( copyMe.getWeightDirection(), weightDirection );
+        this.name = copyMe.getName();
     }
 
+    // Helper method to populate the list of connected Neurons
     private void arrayListHelper( ArrayList<Neuron> copyFrom, ArrayList<Neuron> copyTo ){
         for( Neuron addMe : copyFrom ){ copyTo.add(addMe.clone()); }
     }
 
+    // Helper method to populate the list of weights forwards and backwards
+    private void hashMapHelper( HashMap< String, Double > copyFrom, HashMap< String, Double > copyTo ){
+        for( String key : copyFrom.keySet() ){ copyTo.put( key, copyFrom.get(key) ); }
+    }
+
+    //...................................... SETTERS
     public void setSigmoid(double sigmoid) {
         this.sigmoid = sigmoid;
     }
@@ -52,22 +60,6 @@ public class Neuron implements Cloneable {
         this.threshold = threshold;
     }
 
-//    private HashMap< String, Neuron[] > setupConnectedNodes(){
-//        HashMap< String, Neuron[] > returnMap = new HashMap<>();
-//        returnMap.put( FORWARD, new Neuron[2] );
-//        returnMap.put( BACKWARD, new Neuron[2] );
-//
-//        return returnMap;
-//    }
-//
-//    private HashMap< String, Neuron[] > setupConnectedNodes( Map< String, Neuron[] > copyThisMap ){
-//        HashMap< String, Neuron[] > returnMap = new HashMap<>();
-//        returnMap.put( FORWARD, copyThisMap.get(FORWARD).clone() );
-//        returnMap.put( BACKWARD, copyThisMap.get(BACKWARD).clone() );
-//
-//        return returnMap;
-//    }
-
     public void setAConnectedWeightDirection( String direction, double value ){ weightDirection.put( direction, value ); }
 
     public void setAConnectedNodeDirection( String direction, Neuron node ){
@@ -75,6 +67,7 @@ public class Neuron implements Cloneable {
         else{ backwardNodes.add(node); }
     }
 
+    //...................................... GETTERS
     public double getSigmoid() {
         return sigmoid;
     }
@@ -87,7 +80,7 @@ public class Neuron implements Cloneable {
         return ( direction.toUpperCase().equals(FORWARD) ) ? forwardNodes : backwardNodes;
     }
 
-    public Map<String, Double> getWeightDirection() {
+    public HashMap<String, Double> getWeightDirection() {
         return weightDirection;
     }
 
@@ -99,16 +92,17 @@ public class Neuron implements Cloneable {
         return name;
     }
 
+    // Overridden method used to implementat cloning objects
     public Neuron clone(){
-        try{
-            return (Neuron)super.clone();
-        }
+        try{ return (Neuron) super.clone(); }
         catch( CloneNotSupportedException e ){
             System.out.println( "Cloning not supported" );
             return this;
         }
     }
 
+    // Overridden toString
+    // prints out the neuron name
     public String toString(){
         return String.format( "Neuron %s", name );
     }
